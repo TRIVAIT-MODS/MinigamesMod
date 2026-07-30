aabfc9c63a56d5b53fca3c144de46b7ab367de6e
TRIVAIT <dimitmapper@gmail.com>
2026-07-30 20:34:02 +0400

    Version 1.0.2

--- src/main/java/org/trivait/minigamesmod/minigame/tetris/TetrisScreen.java
+++ src/main/java/org/trivait/minigamesmod/minigame/tetris/TetrisScreen.java
@@ -221,6 +221,7 @@ public class TetrisScreen extends Screen {
         MinecraftClient.getInstance().getSoundManager().play(PositionedSoundInstance.master(SoundEvent.of(Identifier.ofVanilla("entity.pig.ambient")), 1.0F, vol()));
         isNewHighScore = score > MinigameRegistry.getConfig(TetrisConfig.class).tetrisHighScore;
         active = false;
+        tetris.getLeaderboard().doPost(MinecraftClient.getInstance().getGameProfile().name(), score, true);
     }
 
     private boolean checkClear(int y) {
