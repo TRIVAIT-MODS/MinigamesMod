9a2ca42ef77cd111fcec676f9d4e053a4b345a9a
TRIVAIT <dimitmapper@gmail.com>
2026-08-23 13:23:34 +0400

    Version 1.2.1

--- src/main/java/org/trivait/minigamesmod/gui/MinigameListScreen.java
+++ src/main/java/org/trivait/minigamesmod/gui/MinigameListScreen.java
@@ -10,6 +10,7 @@ import org.lwjgl.glfw.GLFW;
 import org.trivait.minigamesmod.api.MinigameDefinition;
 import org.trivait.minigamesmod.api.MinigameRegistry;
 import org.trivait.minigamesmod.gui.widget.ArrowButtonWidget;
+import org.trivait.minigamesmod.gui.widget.MainConfigButton;
 import org.trivait.minigamesmod.gui.widget.MinigameCardWidget;
 import org.trivait.minigamesmod.leaderboard.Leaderboard;
 import org.trivait.minigamesmod.leaderboard.SheetsApi;
@@ -95,6 +96,7 @@ public class MinigameListScreen extends Screen {
         }).dimensions(width / 2 - 35, height / 2 + 70, 70, 20).build();
 
         this.addDrawableChild(leaderboardBtn);
+        this.addDrawableChild(new MainConfigButton(width-20-5, 5));
         updateActive();
     }
 
