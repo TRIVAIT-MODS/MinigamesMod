34eca7cd7271b6b4969758189473ca28eec7644a
TRIVAIT <dimitmapper@gmail.com>
2026-08-25 08:53:05 +0400

    version 1.2.3

--- src/main/java/org/trivait/minigamesmod/gui/widget/MinigamesButton.java
+++ src/main/java/org/trivait/minigamesmod/gui/widget/MinigamesButton.java
@@ -7,15 +7,19 @@ import net.minecraft.client.gui.widget.TextIconButtonWidget;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import org.trivait.minigamesmod.MinigamesMod;
 import org.trivait.minigamesmod.gui.MinigameListScreen;
 
 public class MinigamesButton extends TextIconButtonWidget.IconOnly {
 
     private static final Identifier DEFAULT_TEXTURE = Identifier.of("minigamesmod", "icon/button");
+    private static final Identifier DEFAULT_STATIC_TEXTURE = Identifier.of("minigamesmod", "icon/button_static");
     private static final Identifier HOVER_TEXTURE = Identifier.of("minigamesmod", "icon/button_hover");
+    private static final Identifier HOVER_STATIC_TEXTURE = Identifier.of("minigamesmod", "icon/button_hover_static");
+    private static final Identifier EMPTY = Identifier.of("minigamesmod", "icon/empty");
 
     public MinigamesButton(int x, int y) {
-        super(20, 20, Text.empty(), 16, 16, DEFAULT_TEXTURE, button -> {
+        super(20, 20, Text.empty(), 16, 16, EMPTY, button -> {
             MinecraftClient client = MinecraftClient.getInstance();
             client.setScreen(new MinigameListScreen(client.currentScreen));
         }, null);
@@ -25,7 +29,7 @@ public class MinigamesButton extends TextIconButtonWidget.IconOnly {
 
     @Override
     public void renderWidget(DrawContext context, int mouseX, int mouseY, float delta) {
-        Identifier currentTexture = this.isSelected() ? HOVER_TEXTURE : DEFAULT_TEXTURE;
+        Identifier currentTexture = MinigamesMod.CONFIG.staticIconButton ? this.isSelected() ? HOVER_STATIC_TEXTURE : DEFAULT_STATIC_TEXTURE : this.isSelected() ? HOVER_TEXTURE : DEFAULT_TEXTURE;
         super.renderWidget(context, mouseX, mouseY, delta);
         int i = this.getX() + this.getWidth() / 2 - this.textureWidth / 2;
         int j = this.getY() + this.getHeight() / 2 - this.textureHeight / 2;
