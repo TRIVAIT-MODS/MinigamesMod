c10b0e4a2e153839a016fae5f4c3f7559345945b
TRIVAIT <dimitmapper@gmail.com>
2026-09-24 20:49:50 +0400

    Version 1.3.0

--- src/main/java/org/trivait/minigamesmod/minigame/sudoku/SudokuVisibleConfig.java
+++ src/main/java/org/trivait/minigamesmod/minigame/sudoku/SudokuVisibleConfig.java
@@ -3,8 +3,6 @@ package org.trivait.minigamesmod.minigame.sudoku;
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
 import me.shedaniel.autoconfig.annotation.ConfigEntry;
-import org.trivait.minigamesmod.minigame.tetris.HardDropMode;
-
 @Config(name = "minigames/sudoku_ui")
 public class SudokuVisibleConfig implements ConfigData {
     @ConfigEntry.Gui.EnumHandler(option = ConfigEntry.Gui.EnumHandler.EnumDisplayOption.BUTTON)
