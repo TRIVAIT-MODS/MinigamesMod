975b84ac87986f203a74aa3da1d7d8e76e573ecb
TRIVAIT <dimitmapper@gmail.com>
2026-09-24 21:12:15 +0400

    Version 1.3.0

--- src/main/java/org/trivait/minigamesmod/MinigamesMod.java
+++ src/main/java/org/trivait/minigamesmod/MinigamesMod.java
@@ -18,6 +18,7 @@ import org.trivait.minigamesmod.minigame.cookieclicker.CookieClicker;
 import org.trivait.minigamesmod.minigame.game2048.Game2048;
 import org.trivait.minigamesmod.minigame.minesweeper.MinesweeperGame;
 import org.trivait.minigamesmod.minigame.minesweeper.game.SavedGame;
+import org.trivait.minigamesmod.minigame.solitaire.Solitaire;
 import org.trivait.minigamesmod.minigame.sudoku.Sudoku;
 import org.trivait.minigamesmod.minigame.tetris.Tetris;
 import org.trivait.minigamesmod.minigame.dino.GoogleDino;
@@ -45,6 +46,7 @@ public class MinigamesMod implements ClientModInitializer {
         MinigameRegistry.register(new Sudoku());
         MinigameRegistry.register(new BubbleShooter());
         MinigameRegistry.register(new CookieClicker());
+        MinigameRegistry.register(new Solitaire());
 
         FabricLoader.getInstance().getModContainer("minigamesmod").ifPresent(s ->
                 ResourceManagerHelper.registerBuiltinResourcePack(Identifier.fromNamespaceAndPath("minigamesmod", "minigames"),
