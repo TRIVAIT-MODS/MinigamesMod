82c82fc1a246532649036f0471b01d168dd43d28
TRIVAIT <dimitmapper@gmail.com>
2026-09-24 21:17:46 +0400

    Version 1.3.0

---
+++ src/main/java/org/trivait/minigamesmod/minigame/solitaire/Solitaire.java
@@ -0,0 +1,53 @@
+package org.trivait.minigamesmod.minigame.solitaire;
+
+import me.shedaniel.autoconfig.ConfigData;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.Identifier;
+import net.minecraft.sounds.SoundEvents;
+import org.jetbrains.annotations.Nullable;
+import org.trivait.minigamesmod.MinigamesMod;
+import org.trivait.minigamesmod.api.AbstractMinigame;
+import org.trivait.minigamesmod.api.MinigameRegistry;
+import org.trivait.minigamesmod.api.PlayingSoundManager;
+import org.trivait.minigamesmod.leaderboard.Leaderboard;
+
+import java.util.List;
+
+public class Solitaire extends AbstractMinigame {
+    public Solitaire() {
+        super("solitaire",
+                Component.translatable("minigame.solitaire.title"),
+                Identifier.fromNamespaceAndPath(MinigamesMod.MOD_ID, "textures/minigame/solitaire_icon.png")
+        );
+    }
+
+    @Override
+    public @Nullable Class<? extends ConfigData> getConfigClass() {
+        return SolitaireConfig.class;
+    }
+
+    @Override
+    public @Nullable Class<? extends ConfigData> getVisibleConfigClass() {
+        return SolitaireVisibleConfig.class;
+    }
+
+    @Override
+    public @Nullable Leaderboard getLeaderboard() {
+        return new Leaderboard("Solitaire", null, false);
+    }
+
+    @Override
+    public Screen createScreen(Screen parent) {
+        return new SolitaireScreen(parent, this);
+    }
+
+    @Override
+    public void onWin() {
+        PlayingSoundManager.playSound(SoundEvents.UI_TOAST_CHALLENGE_COMPLETE, 1.0f, vol());
+    }
+
+    private float vol() {
+        return PlayingSoundManager.vol(MinigameRegistry.getConfig(SolitaireVisibleConfig.class).volume);
+    }
+}
---
+++ src/main/java/org/trivait/minigamesmod/minigame/solitaire/SolitaireTextures.java
@@ -0,0 +1,21 @@
+package org.trivait.minigamesmod.minigame.solitaire;
+
+import net.minecraft.resources.Identifier;
+import org.trivait.minigamesmod.MinigamesMod;
+
+public class SolitaireTextures {
+    public static final Identifier GUI_TEXTURE = Identifier.fromNamespaceAndPath(MinigamesMod.MOD_ID, "textures/minigame/solitaire/gui.png");
+    public static final Identifier OVERLAY_CARD = Identifier.fromNamespaceAndPath(MinigamesMod.MOD_ID, "textures/minigame/solitaire/overlay.png");
+    public static final Identifier CLOSED_CARD = Identifier.fromNamespaceAndPath(MinigamesMod.MOD_ID, "textures/minigame/solitaire/closed.png");
+
+    public static Identifier cardTexture(SolitaireGame.Suit suit, int rank) {
+        String value = switch (rank) {
+            case 1 -> "ace";
+            case 11 -> "joker";
+            case 12 -> "queen";
+            case 13 -> "king";
+            default -> Integer.toString(rank);
+        };
+        return Identifier.fromNamespaceAndPath(MinigamesMod.MOD_ID, "textures/minigame/solitaire/" + suit.textureName + "_" + value + ".png");
+    }
+}
